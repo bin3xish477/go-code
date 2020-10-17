d1ddef0537a5464b3a865313af2e75ea2cf4ca92
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-10-17 18:24:14 -0400

    Updating WindowsRegistry directory

--- WindowsRegistry/Introduction/regget.go
+++ WindowsRegistry/Introduction/regget.go
@@ -12,9 +12,9 @@ func print(args interface{}) {
 	fmt.Println(args)
 }
 
-func check(e error) bool {
+func check(e error, errMsg string) bool {
 	if e != nil {
-		print("An error occured ...")
+		print(errMsg)
 		os.Exit(1)
 	}
 	return true
@@ -41,6 +41,33 @@ func openKey(hive registry.Key, subkey string, access uint32) registry.Key {
 	return key
 }
 
+func getComputerInfo() {
+	key := openKey(
+		registry.LOCAL_MACHINE,
+		`SOFTWARE\Microsoft\Windows NT\CurrentVersion`,
+		registry.ALL_ACCESS,
+	)
+	defer key.Close()
+
+	blu := color.New(color.FgBlue)
+	boldBlue := blu.Add(color.Bold)
+	boldBlue.Println("◎ ☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶ Computer Build Info ☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶ ◎")
+
+	productName, _, err := key.GetStringValue("ProductName")
+	check(err, "ProductName value not found in registry...")
+	print("Product Name : " + productName)
+	currentVersion, _, err := key.GetStringValue("CurrentVersion")
+	check(err, "CurrentVersion value not found in registry...")
+	print("Current Version : " + currentVersion)
+	currentBuildNumber, _, err := key.GetStringValue("CurrentBuildNumber")
+	check(err, "CurrentBuildNumber Value not found in registry...")
+	print("Build Number : " + currentBuildNumber)
+	registeredOwner, _, err := key.GetStringValue("RegisteredOwner")
+	check(err, "RegisteredOwner value not found in registry...")
+	print("Registered Owner")
+	print("")
+}
+
 func getInstalledApps() {
 	key := openKey(
 		registry.LOCAL_MACHINE,
@@ -53,7 +80,10 @@ func getInstalledApps() {
 	subkeys, err := key.ReadSubKeyNames(int(numOfSubKeys))
 	check(err)
 
-	color.Red("◎ ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡ Installed Applications ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡ ◎")
+	rd := color.New(color.FgRed)
+	boldRed := rd.Add(color.Bold)
+	boldRed.Println("◎ ☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶ Installed Applications ☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶ ◎")
+
 	for _, skey := range subkeys {
 		k := openKey(
 			registry.LOCAL_MACHINE,
@@ -80,15 +110,50 @@ func getEnVars() {
 		For User environment variables:
 		HKEY_CURRENT_USER\Environment
 	*/
+	key := openKey(
+		registry.LOCAL_MACHINE,
+		`SYSTEM\CurrentControlSet\Control\Session Manager\Environment`,
+		registry.ALL_ACCESS,
+	)
+	defer key.Close()
 
+	_, numOfValues := getNumberOfSubKeysAndValues(key)
+	environmentVariables, err := key.ReadValueNames(int(numOfValues))
+	check(err)
+
+	grn := color.New(color.FgGreen)
+	boldGreen := grn.Add(color.Bold)
+	boldGreen.Println("\n◎ ☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶ Environment Variables ☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶☶ ◎")
+
+	for _, envar := range environmentVariables {
+		envarValue, _, err := key.GetStringValue(envar)
+		check(err)
+		print(envar + " ☰☰ " + envarValue)
+	}
+	print("")
 }
 
 func getStartUpApps() {
 
 }
 
+func getJumpLists() {
+
+}
+
+func getLNKFiles() {
+
+}
+
+func getShellBags() {
+
+}
+
 func main() {
+	getComputerInfo()
 	getInstalledApps()
 	getEnVars()
 	getStartUpApps()
+	getJumpLists()
+	getLNKFiles()
 }
