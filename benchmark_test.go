4538e0ef3159e342c4b5b3fbcbedf6a73b2c8f22
Alexis Rodriguez <44281620+bin3xish477@users.noreply.github.com>
2021-12-08 03:59:00 -0500

    Create benchmark_test.go

---
+++ benchmark_test.go
@@ -0,0 +1,32 @@
+package main
+
+/*
+$ go test -bench=. -benchmem
+*/
+
+import (
+        "bytes"
+        "strings"
+        "testing"
+)
+
+//func BenchmarkSprintf(b *testing.B) {
+//var s string
+//for n := 0; n < b.N; n++ {
+//s = fmt.Sprintf("%s%s", s, "a")
+//}
+//}
+
+func BenchmarkBytesBuffer(b *testing.B) {
+        var buf bytes.Buffer
+        for n := 0; n < b.N; n++ {
+                buf.WriteString("a")
+        }
+}
+
+func BenchmarkStringsBuilders(b *testing.B) {
+        var builder strings.Builder
+        for n := 0; n < b.N; n++ {
+                builder.WriteString("a")
+        }
+}
